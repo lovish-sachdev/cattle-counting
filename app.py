82ee3e10c6f4074eb1f28b41c083da8a964938cf
lovish-sachdev <137512011+lovish-sachdev@users.noreply.github.com>
2024-06-19 12:50:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,11 +110,11 @@ if uploaded_video is not None: # run only when user uploads video
     vidcap = cv2.VideoCapture(vid) # load video from disk
     cur_frame = 0
     success = True
-
+    st_image=st.empty()
     while success:
         success, frame = vidcap.read() # get next frame from video
         if cur_frame % frame_skip == 0: # only analyze every n=300 frames
             print('frame: {}'.format(cur_frame)) 
             pil_img = Image.fromarray(frame) # convert opencv frame (with type()==numpy) into PIL Image
-            st.image(pil_img)
+            st_image.image(pil_img)
         cur_frame += 1
